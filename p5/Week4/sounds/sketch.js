a656838bba54bb2f39cc72f151b230e4657f2ff2
ethancossio <59842566+ethancossio@users.noreply.github.com>
2020-02-13 09:21:36 -0600

    THURSDAY ASSIGNMENT

--- p5/Week4/sounds/sketch.js
+++ p5/Week4/sounds/sketch.js
@@ -4,6 +4,11 @@ function preload() {
   SPESS = loadSound('assets/SPESSMEHREENS.mp4');
 }
 function setup() {
+  //function setup() {
+//let c = createCanvas(100, 100);
+//background(255, 0, 0);
+//saveCanvas(c, 'myCanvas', 'jpg');
+
   // put setup code here
   createCanvas(720, 200) ;
   background(255,0,0);
