658839ff63f6743fd88bf0075e675bc8e26af970
ethancossio <59842566+ethancossio@users.noreply.github.com>
2020-03-04 14:16:11 -0600

    Got everything except sound

--- p5/Week7/mp2/sketch.js
+++ p5/Week7/mp2/sketch.js
@@ -1,57 +1,354 @@
 var myState = 0;
 var timer = 0;
+var Hound;
+var Lich;
+var Revenant;
+var Crossbowman;
+var MonsterFace;
+//var Dracula;
+//var EvilLaugh;
+//var MonsterGrowl;
+var x = 0;
+var gif_loadImg, gif_createImg;
+var gif_loadImg2, gif_createImg2;
+var gif_loadImg3, gif_createImg3;
+var gif_loadImg4, gif_createImg4;
+var gif_loadImg5, gif_createImg5;
+var gif_loadImg6, gif_createImg6;
+
+function preload() {
+
+gif_loadImg = loadImage("assets/VampireFlying.gif");
+  gif_createImg = createImg("assets/VampireFlying.gif");
+
+  gif_loadImg2 = loadImage("assets/MonsterFace.gif");
+  gif_createImg2 = createImg("assets/MonsterFace.gif")
+
+  gif_loadImg3 = loadImage("assets/LeftHand.gif");
+  gif_createImg3 = createImg("assets/LeftHand.gif");
+
+  gif_loadImg4 = loadImage("assets/RightHand.gif");
+  gif_createImg4 = createImg("assets/RightHand.gif");
+
+  gif_loadImg5 = loadImage("assets/Soul.gif");
+  gif_createImg5 = createImg("assets/Soul.gif");
+
+  gif_loadImg6 = loadImage("assets/Soul.gif");
+  gif_createImg6 = createImg("assets/Soul.gif");
+
+  gif_loadImg7 = loadImage("assets/Soul.gif");
+  gif_createImg7 = createImg("assets/Soul.gif");
+
+  gif_loadImg8 = loadImage("assets/FastSoul.gif");
+  gif_createImg8 = createImg("assets/FastSoul.gif");
+
+  gif_loadImg9 = loadImage("assets/FastSoul.gif");
+  gif_createImg9 = createImg("assets/FastSoul.gif");
+
+  gif_loadImg10 = loadImage("assets/FastSoul.gif");
+  gif_createImg10 = createImg("assets/FastSoul.gif");
+
+//  Dracula = loadSound('assets/Dracula.mp4');
+//  EvilLaugh = loadSound('assets/EvilLaugh.mp4');
+//  MonsterGrowl = loadSound('assets/MonsterGrowl.mp4');
+
+}
+
 
 function setup() {
   // put setup code here
   createCanvas(800, 800);
+  background('black');
+  Hound = loadImage('assets/Hound.png');
+  Lich = loadImage ('assets/Lich.png');
+  Revenant = loadImage ('assets/Revenant.png');
+  Crossbowman = loadImage ('assets/Crossbowman.png');
+  MonsterFace = loadImage ('assets/MonsterFace.png');
+  STRANGERCREATURE = loadFont('assets/STRANGERCREATURE.ttf');
+
+  //gif width & height
+  gif_createImg.position(-5000,-5000);
+  gif_width = gif_createImg.width;
+  gif_height = gif_createImg.height;
+  print("gif width: " + gif_width) ;
+  print("gif height: " + gif_height);
+  gif_createImg.size(gif_width/2, gif_height/2);
+
+
+    gif_createImg2.position(-5000,-5000);
+    gif_width = gif_createImg2.width;
+    gif_height = gif_createImg2.height;
+    print("gif width: " + gif_width) ;
+    print("gif height: " + gif_height);
+    gif_createImg2.size(gif_width/2, gif_height/2);
+
+    gif_createImg3.position(-5000,-5000);
+  gif_width = gif_createImg3.width;
+  gif_height = gif_createImg3.height;
+  print("gif width: " + gif_width) ;
+  print("gif height: " + gif_height);
+    gif_createImg3.size(gif_width/2, gif_height/2);
+
+  gif_createImg4.position(-5000,-5000);
+  gif_width = gif_createImg4.width;
+  gif_height = gif_createImg4.height;
+  print("gif width: " + gif_width) ;
+  print("gif height: " + gif_height);
+  gif_createImg4.size(gif_width/2, gif_height/2);
+
+  gif_createImg5.position(-5000,-5000);
+  gif_width = gif_createImg5.width;
+  gif_height = gif_createImg5.height;
+  print("gif width: " + gif_width) ;
+  print("gif height: " + gif_height);
+    gif_createImg5.size(gif_width/2, gif_height/2);
+
+gif_createImg6.position(-5000,-5000);
+  gif_width = gif_createImg6.width;
+  gif_height = gif_createImg6.height;
+  print("gif width: " + gif_width) ;
+  print("gif height: " + gif_height);
+    gif_createImg6.size(gif_width/2, gif_height/2);
+
+    gif_createImg7.position(-5000,-5000);
+      gif_width = gif_createImg7.width;
+      gif_height = gif_createImg7.height;
+      print("gif width: " + gif_width) ;
+      print("gif height: " + gif_height);
+        gif_createImg7.size(gif_width/2, gif_height/2);
+
+        gif_createImg8.position(-5000,-5000);
+          gif_width = gif_createImg8.width;
+          gif_height = gif_createImg8.height;
+          print("gif width: " + gif_width) ;
+          print("gif height: " + gif_height);
+            gif_createImg8.size(gif_width/2, gif_height/2);
+
+            gif_createImg9.position(-5000,-5000);
+              gif_width = gif_createImg9.width;
+              gif_height = gif_createImg9.height;
+              print("gif width: " + gif_width) ;
+              print("gif height: " + gif_height);
+                gif_createImg9.size(gif_width/2, gif_height/2);
+
+                gif_createImg10.position(-5000,-5000);
+                  gif_width = gif_createImg10.width;
+                  gif_height = gif_createImg10.height;
+                  print("gif width: " + gif_width) ;
+                  print("gif height: " + gif_height);
+                    gif_createImg10.size(gif_width/2, gif_height/2);
+
+  // change size
+
+
+
 }
 
 function draw() {
   // put drawing code here
   switch (myState) {
     case 0:
-      background('red');
-      text("Tehre is noh time to beh lohst! Batul Brothas!", 100, 100);
-      timer = timer + 1;
-      if (timer > 200) {
-        myState = 1 ;
-        timer = 0;
-      }
+      background('black');
+        fill('white');
+        textSize(25);
+      text("Let's start this presentation with a quiz!", 150, 100);
+
       break;
+
     case 1:
-      background('green');
-      text("Spehss Mahrens, todeh the enemeh is at oua doar! We know oua duteh and we will do eet. We fight for our honor as Blud Rehvens,as SPESS MAHRENS, and we fight in the nehme of the Empra!", 100, 100);
+      background('black');
+      text("Which one of these is not like the other?", 150, 100);
+      image(Hound, 100, 400,200,200);
+      image(Lich, 100, 150,200,200);
+      image(Revenant, 400, 150,200,200);
+      image(Crossbowman, 400, 400,200,200);
+
+      TimerText();
+    //  push();
+    //  translate(665,-103);
+  //    rotate(radians(-265))
+    //      TimerText();
+
+  //  pop();
       timer = timer + 1;
-      if (timer > 200) {
+      if (timer > 500) {
         myState = 2 ;
         timer = 0;
       }
       break;
 
     case 2:
-      background('red');
-      text("deep strikes", 100, 100);
-      timer = timer + 1;
-      if (timer > 200) {
-        myState = 3 ;
-        timer = 0;
-      }
+    background('black');
+    text("That's right! It's", 200, 100);
+    push();
+    fill('green');
+    textSize(25);
+    text("Crossbowman!", 375, 100);
+pop();
+
+    push();
+    fill('purple');
+    textSize (25);
+    text("How dare you?!", 150,150);
+    pop();
+
+    push();
+    fill('turquoise');
+    textSize (25);
+    text("I WILL TEAR YOUR SOUL APART!!!", 380,140);
+    pop();
+
+    push();
+    fill('Grey');
+    textSize (25);
+    text("Arf?", 150,400);
+    pop();
+
+    push();
+    fill('Green');
+    textSize (25);
+    text("Blaaaaaaaaaarrrrrggggggghhhhh...!", 375,380);
+    pop();
+
+    image(Hound, 100, 400,200,200);
+    image(Lich, 100, 150,200,200);
+    image(Revenant, 400, 150,200,200);
+    image(Crossbowman, 400, 400,200,200);
+
       break;
 
     case 3:
-     background('green');
-     text("SPESS SPESS SPESS",100, 100);
-      break;
-      timer = timer + 1;
-      if (timer > 200) {
-        myState = 4 ;
-        timer = 0;
-      }
+    background('black');
+     image(Revenant, 275, 400,200,300);
+     image(Lich, 0, 400,200,300);
+     image(Hound, 550, 400,200,300);
+
+     push();
+     fill('Purple');
+     textSize(25);
+     text("I am the most intelligent out of this lot!",0, 375 );
+     pop();
+
+     push();
+     fill('turquoise');
+     textSize(25);
+     text("I LITERALLY HAVE NO FLESH OR BONES!!!",200, 325 );
+     pop();
+         push();
+         fill('Grey');
+         textSize(25);
+          text("Arf arf!",550, 400);
+          pop();
+    break;
 
     case 4:
-      background('red');
-      text("We are the spehss mahrens! WE ARE THE EMPRA'S FUREH!", 100, 100);
+    background('black');
+      image(Revenant, 275, 400,200,300);
+      image(Lich, 0, 400,200,300);
+      image(Hound, 550, 400,200,300);
+
+      push();
+      fill('Purple');
+      textSize(25);
+      text("I command you to pick me instead!",0, 375 );
+      pop();
+
+      push();
+      fill('turquoise');
+      textSize(25);
+      text("PICK ME OR YOU WILL KNOW MY PAIN!!!",200, 325 );
+      pop();
+          push();
+          fill('Grey');
+          textSize(25);
+           text("Arf Aaaaarrrrfff!",550, 400);
+           pop();
       break;
+
+      case 5:
+      background('black');
+      fill('red');
+      text("Are my subjects being disorderly", 65,200);
+      textFont(STRANGERCREATURE, 50);
+      image(Revenant, 275, 400,200,300);
+      image(Lich, 0, 400,200,300);
+      image(Hound, 550, 400,200,300);
+
+
+      break;
+
+      case 6:
+      background('black');
+      image(Revenant, 275, 500,200,300);
+      image(Lich, 0, 500,200,300);
+      image(Hound, 550, 500,200,300);
+      fill('red');
+      text("It is off to oblivion with you lot", 50,100);
+      textFont(STRANGERCREATURE, 50);
+      gif_createImg.position(250, 140);
+
+      break;
+
+      case 7:
+
+      background('black');
+      fill('red');
+      text("Klaatu barada nitko", 175,100);
+      textFont(STRANGERCREATURE, 50);
+
+      gif_createImg5.position(0, 500);
+            gif_createImg6.position(250, 500);
+                  gif_createImg7.position(500, 500);
+
+                  break;
+
+                  case 8:
+                  background('black');
+                  fill('red');
+                  text("Good thing too because its dinner time", 15,100);
+                  textFont(STRANGERCREATURE, 50);
+
+                  gif_createImg5.position(0, 500);
+                        gif_createImg6.position(250, 500);
+                              gif_createImg7.position(500, 500);
+
+
+                              break;
+                              case 9:
+
+                              background('black');
+                              fill('red');
+                              text("The takeaway is don't be a sore loser", 15,100);
+                              textFont(STRANGERCREATURE, 50);
+                              gif_createImg2.position(250, 110);
+                                    gif_createImg3.position(500, 200);
+                                          gif_createImg4.position(0, 200);
+                              gif_createImg8.position(0, 500);
+                                    gif_createImg9.position(250, 500);
+                                          gif_createImg10.position(500, 500);
+
+
+
+    //  image(gif_loadImg, 50, 50);
+
+ // updates animation frames by using an html
+ // img element, positioning it over top of
+ // the canvas.
+ //gif_createImg.position(50, 350);
+
+      //image(gif_loadImg, 50, 50);
+
+       // updates animation frames by using an html
+       // img element, positioning it over top of
+       // the canvas.
+
+
+      break;
+
+      case 7:
+      image(Revenant, 275, 400,200,300);
+      image(Lich, 0, 400,200,300);
+      image(Hound, 550, 400,200,300);
 
   }
 
@@ -59,7 +356,22 @@ function draw() {
 
 function mouseReleased() {
   myState = myState + 1;
-  if (myState > 4) {
-      myState = 0;
-    }
+  //if (myState > 7) {
+    //  myState = 0;
+  //  }
 }
+function TimerText(){
+    push();
+    fill('black');
+    rect(x, 700, 540, 50);
+    fill('red');
+    textSize(25);
+    text("Hurry before I reach the other side of the screen!", x, 730);
+
+    x = x + 0.520;
+    if (x > width) {
+      x = 0;
+    }
+
+    pop();
+  }
